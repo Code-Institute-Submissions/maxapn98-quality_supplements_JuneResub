948b0881a9a10ae67bb1923d1be7357ffc8e649f
maxapn98 <Nikolamerano@gmail.com>
2022-04-27 11:12:25 +0000

    add produtct app, models and media

---
+++ products/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+
+class Category(models.Model):
+    product_category = models.CharField(max_length=254)
+
+    def __str__(self):
+        return self.product_category
+
+
+
+class Product(models.Model):
+    product_category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    brand_name = models.CharField(max_length=254, null=True, blank=True)
+    product_name = models.CharField(max_length=254)
+    product_description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    
+
+    def __str__(self):
+        return self.brand_name
