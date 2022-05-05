ece31223f88c95cf4f0c8405a3d5bb483bbfcfe8
maxapn98 <Nikolamerano@gmail.com>
2022-05-05 10:50:18 +0000

    update settings

--- qs/settings.py
+++ qs/settings.py
@@ -194,8 +194,8 @@ if "USE_AWS" in os.environ:
     }
 
 # Bucket config
-AWS_STORAGE_BUCKET_NAME = "milestone-4-delisw"
-AWS_S3_REGION_NAME = "eu-west-2"
+AWS_STORAGE_BUCKET_NAME = "quality-supplements
+AWS_S3_REGION_NAME = "EU (Paris) eu-west-3"
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
