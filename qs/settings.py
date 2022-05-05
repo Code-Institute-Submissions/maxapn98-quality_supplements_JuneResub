e9a52eb41c237fc7d3dab089c2d20d52782e546c
maxapn98 <Nikolamerano@gmail.com>
2022-05-05 08:56:52 +0000

    removed secret key and set debug

--- qs/settings.py
+++ qs/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kv()+z7lh$-#e9gn&4@=9v(t8hcvg66aj(7$g19v90j=p)*d^u'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['quality-supplements.herokuapp.com','localhost']
 
