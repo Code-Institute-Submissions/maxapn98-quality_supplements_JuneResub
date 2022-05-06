0c19a48d649ea054ecf0b823d5b6a6abaa4cced0
maxapn98 <Nikolamerano@gmail.com>
2022-05-06 22:35:51 +0000

    update home page

--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Category(models.Model):
@@ -28,3 +29,9 @@ class Product(models.Model):
 
     def __str__(self):
         return self.product_name
+
+
+class Review(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100, null=True, blank=True)
+    comment = models.CharField(max_length=500, null=True, blank=True)
