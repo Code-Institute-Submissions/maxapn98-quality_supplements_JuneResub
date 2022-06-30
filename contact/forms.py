67f7a78eef6ab0a38c22de8b3fa814b0da367384
maxapn98 <Nikolamerano@gmail.com>
2022-06-30 08:34:51 +0000

    Update Contact form to use Message model for later is_valid func

--- contact/forms.py
+++ contact/forms.py
@@ -1,19 +1,22 @@
+from attr import fields
 from django import forms
-
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit, Field
+
+from contact.models import Message
 
 
-class ContactForm(forms.Form):
-
-    first_name = forms.CharField(required=True)
-    last_name = forms.CharField(required=True)
-    from_email = forms.EmailField(required=True)
-    subject = forms.CharField(required=True)
-    content = forms.CharField(widget=forms.Textarea, required=True)
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Message
+        fields = ("first_name", "last_name", "email", "subject", "message")
 
     def __init__(self, *args, **kwargs):
-        super(ContactForm, self).__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
+        first_name = forms.CharField(required=True)
+        last_name = forms.CharField(required=True)
+        email = forms.EmailField(required=True)
+        subject = forms.CharField(required=True)
+        message = forms.CharField(widget=forms.Textarea, required=True)
         self.helper = FormHelper()
 
         self.helper.form_method = 'post'
