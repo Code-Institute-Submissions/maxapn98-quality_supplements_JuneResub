b008549315f90161fa9aebcf458366702013566e
maxapn98 <Nikolamerano@gmail.com>
2022-06-30 18:01:40 +0000

    Register Message model

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
+from contact.models import Message
+
 # Register your models here.
+
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = (
+        'subject',
+        'first_name',
+        'last_name',
+        'email',
+        'message',
+    )
+
+    ordering = ('created_at',)
+
+
+admin.site.register(Message)
