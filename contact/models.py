29fd4ffe41bd8fa8d2cc038c526faca1adeef3d2
maxapn98 <Nikolamerano@gmail.com>
2022-06-30 08:32:46 +0000

    Update Message model fields and str method

--- contact/models.py
+++ contact/models.py
@@ -2,8 +2,14 @@ from django.db import models
 
 # Create your models here.
 
+
 class Message(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    email = models.EmailField(max_length=254)
-    message = models.TextField(max_length=300)
+    first_name = models.CharField(max_length=50, null=False, blank=False)
+    last_name = models.CharField(max_length=50, null=False, blank=False)
+    subject = models.CharField(
+        max_length=100, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    message = models.TextField(max_length=300, null=False, blank=False)
+
+    def __str__(self):
+        return f'Subject: {self.subject} Email: {self.email}'
