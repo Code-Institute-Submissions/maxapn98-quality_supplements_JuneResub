4fad07026d21edbb505b23b48a3cb1bff081e78d
maxapn98 <Nikolamerano@gmail.com>
2022-07-01 01:27:06 +0000

    Delete conditional logic for redirecting non staff members

--- products/views.py
+++ products/views.py
@@ -74,9 +74,6 @@ def product_detail(request, product_id):
 @staff_member_required
 def add_product(request):
     """ Add a product to the store """
-    if not request.user.is_superuser:
-        messages.error(request, 'Sorry, only store owners can do that.')
-        return redirect(reverse('home'))
 
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
@@ -102,9 +99,6 @@ def add_product(request):
 @staff_member_required
 def edit_product(request, product_id):
     """ Edit a product in the store """
-    if not request.user.is_superuser:
-        messages.error(request, 'Sorry, only store owners can do that.')
-        return redirect(reverse('home'))
 
     product = get_object_or_404(Product, pk=product_id)
     if request.method == 'POST':
@@ -133,9 +127,6 @@ def edit_product(request, product_id):
 @staff_member_required
 def delete_product(request, product_id):
     """ Delete a product from the store """
-    if not request.user.is_superuser:
-        messages.error(request, 'Sorry, only store owners can do that.')
-        return redirect(reverse('home'))
 
     product = get_object_or_404(Product, pk=product_id)
     product.delete()
