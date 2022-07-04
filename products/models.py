ab717dc6603db41d3163e13d751cbbcc808c4ab8
maxapn98 <Nikolamerano@gmail.com>
2022-07-04 22:29:19 +0000

    Remove related name from Review model field

--- products/models.py
+++ products/models.py
@@ -32,7 +32,7 @@ class Product(models.Model):
 
 
 class Review(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name ='comments')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
     user =  models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField(max_length=500, null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
