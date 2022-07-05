34304d2ff80ce2ca2498325e628c41812b9181f9
maxapn98 <Nikolamerano@gmail.com>
2022-07-05 00:22:15 +0000

    Update edit product form context and style layers

--- products/forms.py
+++ products/forms.py
@@ -3,7 +3,6 @@ from .models import Product, Category,Review
 
 
 class ProductForm(forms.ModelForm):
-
     class Meta:
         model = Product
         fields = '__all__'
@@ -11,9 +10,6 @@ class ProductForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         categories = Category.objects.all()
-
-        for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'border-black rounded-0'
 
 
 class ReviewForm(forms.ModelForm):
